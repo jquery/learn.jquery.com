010c43a1764f36ea274bc504f53ebbbbf9882402
Sean Suggs <ssuggs@digitalfunction.com>
2011-10-06 15:47:53 -0500

    initial add of jsFiddle demos

---
+++ code/exercises/demos/CHAPTER-7/LoadContentUsingJSON/demo.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    /* jQuery Code Here */
+    
+/////////////
+//EXERCISES//
+/////////////
+        
+console.log("\n \n Load Content Using JSON//////////");
+//Load Content Using JSON
+// URL to external content ---   /gh/get/response.json/Suggsgested/web-learn-jquery-com/tree/master/code/exercises/demos/CHAPTER-7/LoacContentUsingJSON/
+
+
+
+});
