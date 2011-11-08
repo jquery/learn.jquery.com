15fb9aa923a5f768810a2aaa9ea99370ffe74ff1
Mickael Daniel <daniel.mickael@gmail.com>
2011-11-08 20:47:27 +0100

    main refactoring, put helpers/steps function across different modules

---
+++ fiddles/lib/fiddles/helpers.js
@@ -0,0 +1,51 @@
+
+var util = require('util'),
+  exec = require('child_process').exec,
+  mime = require('mime'),
+  path = require('path'),
+  fs = require('fs');
+
+// #### helpers
+var inspect = exports.inspect = function inspect(data) {
+  process.stdout.write(util.inspect(data, false, 4, true) + '\n');
+};
+
+var mkdirp = exports.mkdirp = function mkdirp(path, cb) {
+  exec('mkdir -p ' + path, cb);
+};
+
+var error = exports.error = function error(err) {
+  console.error(err);
+  process.exit(1);
+};
+
+var tmpl = exports.tmpl = function tmpl(s,d) {
+  return s.replace(/\{([a-z]+)\}/g, function(w,m) {
+    return d[m] || '';
+  });
+};
+
+var b64 = exports.b64 = function b64(path) {
+  return tmpl('data:{mediatype};base64,{hash}', {
+    mediatype: mime.lookup(path),
+    hash: fs.readFileSync(path, 'base64')
+  });
+};
+
+var ghHint = exports.ghHint = function ghHint(config) {
+  return [
+    'Failed to load github ' + config + ' from git config.',
+    '',
+    'Run these two commands:',
+    '',
+    'git config --global github.user "your-github-username"',
+    'git config --global github.token "your-github-token"',
+    '',
+    '',
+    'To verify that they have been set, use:',
+    '',
+    'git config --get github.user',
+    'git config --get github.token',
+    ''
+  ].join('\n');
+};
