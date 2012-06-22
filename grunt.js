1ad2c86dc0641771ec90a4912daf8709daee0bfd
adam j. sontag <ajpiano@ajpiano.com>
2012-06-21 23:01:50 -0400

    preliminary conformance with node-wordpress

---
+++ grunt.js
@@ -0,0 +1,45 @@
+module.exports = function( grunt ) {
+
+grunt.loadNpmTasks( "grunt-clean" );
+grunt.loadNpmTasks( "grunt-html" );
+grunt.loadNpmTasks( "grunt-wordpress" );
+grunt.loadNpmTasks( "grunt-jquery-content" );
+
+grunt.initConfig({
+	clean: {
+		folder: "dist/"
+	},
+	htmllint: {
+		resources: "resources/*.html"
+	},
+	jshint: {
+		options: {
+			undef: true,
+			node: true
+		}
+	},
+	lint: {
+		grunt: "grunt.js"
+	},
+	watch: {
+		pages: {
+			files: "page/**/*.html",
+			tasks: "deploy"
+		}
+	},
+	"build-pages": {
+		all: grunt.file.expandFiles( "page/**/*.html" )
+	},
+	"build-resources": {
+		all: grunt.file.expandFiles( "resources/**/*" )
+	},
+	wordpress: grunt.utils._.extend({
+		dir: "dist/wordpress"
+	}, grunt.file.readJSON( "config.json" ) )
+});
+
+grunt.registerTask( "default", "lint" );
+grunt.registerTask( "build-wordpress", "clean lint build-pages build-resources");
+grunt.registerTask( "deploy", "wordpress-deploy" );
+
+};
