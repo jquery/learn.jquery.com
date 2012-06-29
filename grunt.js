13cb303d9f089d9fa5c935c6766b97631101f512
Jim Wang <turanuk@gmail.com>
2012-06-28 21:53:52 -0700

    Modifying to add nanoc-compile task to grunt

--- grunt.js
+++ grunt.js
@@ -1,9 +1,24 @@
+var cp = require("child_process");
+
 module.exports = function( grunt ) {
 
 grunt.loadNpmTasks( "grunt-clean" );
 grunt.loadNpmTasks( "grunt-html" );
 grunt.loadNpmTasks( "grunt-wordpress" );
 grunt.loadNpmTasks( "grunt-jquery-content" );
+
+grunt.registerTask( "nanoc-compile", "compiles nanoc", function () {
+	console.log( "Doing 'nanoc compile' in '" + __dirname + "'" );
+	var done = this.async();
+	cp.exec( "nanoc compile",  function( error, stdout, stderr ) {
+		console.log( stdout );
+		if ( error !== null ) {
+			console.error( error );
+			process.exit( error.code );
+		}
+		done();
+	});
+});
 
 grunt.initConfig({
 	clean: {
@@ -39,7 +54,7 @@ grunt.initConfig({
 });
 
 grunt.registerTask( "default", "lint" );
-grunt.registerTask( "build-wordpress", "clean lint build-pages build-resources");
+grunt.registerTask( "build-wordpress", "clean lint nanoc-compile build-pages build-resources");
 grunt.registerTask( "deploy", "wordpress-deploy" );
 
 };
