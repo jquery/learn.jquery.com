eb2781cdb78081d7d4a3727e3a091ba8e1c26589
adam j. sontag <ajpiano@ajpiano.com>
2012-10-12 00:48:49 -0400

    begin removing wintersmith dependency and using grunt-jquery-content for markdown processing by removing task from grunt.js, renaming "index.md" files in each "chapter" folder to appropriately named .md file in root to play nice with grunt-jquery-content

--- grunt.js
+++ grunt.js
@@ -7,12 +7,10 @@ grunt.loadNpmTasks( "grunt-clean" );
 grunt.loadNpmTasks( "grunt-html" );
 grunt.loadNpmTasks( "grunt-wordpress" );
 grunt.loadNpmTasks( "grunt-jquery-content" );
-grunt.loadNpmTasks( "grunt-wintersmith" );
 
 grunt.initConfig({
 	clean: {
-		wordpress: "dist/",
-		wintersmith: "page/"
+		wordpress: "dist/"
 	},
 	htmllint: {
 		resources: "resources/*.html"
@@ -28,25 +26,23 @@ grunt.initConfig({
 	},
 	watch: {
 		pages: {
-			files: "page/**/*.html",
+			files: "content/**",
 			tasks: "deploy"
 		}
 	},
 	"build-pages": {
-		all: grunt.file.expandFiles( "page/**/*.html" )
+		all: grunt.file.expandFiles( "content/**" )
 	},
 	"build-resources": {
 		all: grunt.file.expandFiles( "resources/**/*" )
 	},
 	wordpress: grunt.utils._.extend({
 		dir: "dist/wordpress"
-        }, grunt.file.readJSON( "config.json" ) ),
-
-        wintersmith: config
+        }, grunt.file.readJSON( "config.json" ) )
 });
 
 grunt.registerTask( "default", "wordpress-deploy" );
-grunt.registerTask( "build-wordpress", "clean lint wintersmith build-pages build-resources");
+grunt.registerTask( "build-wordpress", "clean lint build-pages build-resources");
 grunt.registerTask( "deploy", "wordpress-deploy" );
 
 };
