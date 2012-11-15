495c41db472f2bff815ec4d3974cc244b15f839e
adam j. sontag <ajpiano@ajpiano.com>
2012-11-15 17:50:57 -0500

    remove legacy code examples, layouts directory

--- code/exercises/tests/tests-jasmine-helper.js
+++
@@ -1,4 +0,0 @@
-/**
- * Use this file for testing code that should run for all tests,
- * or for setup code for all tests.
- */
--- code/solutions/stripe.js
+++
@@ -1,7 +0,0 @@
-(function($) {
-	$.fn.stripe = function(color) {
-		return this.find('tbody tr:odd td').css('backgroundColor', color);
-	};
-})(jQuery);
-
-// usage: $('#myTable').stripe('#cccccc');
