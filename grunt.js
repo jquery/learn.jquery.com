47c2b40c8832547cf46ea0be37a6e73cd250d8ba
Scott González <scott.gonzalez@gmail.com>
2014-12-03 10:10:12 -0500

    Build: General cleanup and removal of grunt-specific APIs

--- grunt.js
+++ grunt.js
@@ -1,4 +1,5 @@
-var rimraf = require( "rimraf" );
+var rimraf = require( "rimraf" ),
+	spawnback = require( "spawnback" );
 
 module.exports = function( grunt ) {
 
@@ -22,150 +23,116 @@ grunt.registerTask( "clean", function() {
 	rimraf.sync( "dist" );
 });
 
-// Process a JSON order file and return an object of page slugs and their ordinal indices
-grunt.registerHelper( "read-order", function( orderFile ) {
-	var order,
-		map = {},
+function getOrderMap() {
+	var map = {},
 		index = 0;
 
-	try {
-		order = JSON.parse( grunt.file.read( orderFile ) );
-	} catch( error ) {
-		grunt.warn( "Invalid order file: " + orderFile );
-		return null;
+	function walk( items, prefix ) {
+		items.forEach(function( item ) {
+			if ( typeof item === "object" ) {
+				var page = Object.keys( item )[ 0 ];
+				map[ prefix + page ] = ++index;
+				walk( item[ page ], prefix + page + "/" );
+			} else {
+				map[ prefix + item ] = ++index;
+			}
+		});
 	}
 
+	walk( require( "./order" ), "" );
 
-	function flatten( item, folder ) {
-		var title,
-		path = folder ? [ folder ] : [];
-
-		if ( grunt.utils._.isObject( item ) ) {
-			title = Object.keys( item )[ 0 ];
-			path.push( title );
-			path = path.join( "/" );
-			map[ path ] = ++index;
-
-			item[ title ].forEach(function( item ) {
-				flatten( item, path );
-			});
-		} else {
-			path.push( item );
-			map[ path.join( "/" ) ] = ++index;
-		}
-	}
-	order.forEach(function( item ) {
-		flatten( item );
-	});
 	return map;
-});
+}
 
-grunt.registerHelper( "contributor-attribution", function( post, fileName, fn ) {
-	var contribs = [],
-		_ = grunt.utils._,
-		parseRE = /^(.*)<(.*)>$/; // could certainly be better.
+function contributorAttribution( post, fileName, callback ) {
+	var contribs,
+		parseRE = /^(.*)<(.*)>$/;
 
 	// Read contributors from git file information
-	grunt.utils.spawn({
-		cmd: "git",
-		args: [
-			"log",
-			"--follow", // Trace history through file rename operations
-			"--diff-filter=AM", // Only consider "Add" and "Modify" operations
-			"--format=%aN <%aE>",
-			fileName
-		]
-	}, function( err, result ) {
-		if ( err ) {
-			grunt.verbose.error();
-			grunt.log.error( err );
-			return;
+	spawnback( "git", [
+		"log",
+		"--follow", // Trace history through file rename operations
+		"--diff-filter=AM", // Only consider "Add" and "Modify" operations
+		"--format=%aN <%aE>",
+		fileName
+	], function( error, result ) {
+		if ( error ) {
+			return callback( error );
 		}
-		// make unique.
-		contribs = _.uniq( result.stdout.split( /\r?\n/g ) );
 
-		// make object { name: 'name', email: 'email@address.com' }
-		contribs.forEach(function(str, idx) {
-			var m = parseRE.exec(str);
-			if ( m ) {
-				contribs[idx] = { name: m[1].trim(), email: m[2] };
-			}
-			else {
-				contribs[idx] = { name: str };
-			}
-		});
+		contribs = result.trimRight().split( /\r?\n/g )
 
-		// Alphabetize by 'last name' (relatively crude)
-		contribs = _.sortBy( contribs, function(a) {
-			return a.name.split(' ').pop().toLowerCase();
-		});
+			// Reduce to a unique list of contributors
+			.filter(function( value, index, array ) {
+				return array.indexOf( value ) === index;
+			})
+
+			// Convert to structured objects
+			.map(function( contributor ) {
+				var matches = parseRE.exec( contributor );
+				return {
+					name: matches[ 1 ].trim(),
+					email: matches[ 2 ]
+				};
+			})
+
+			// Alphabetize by 'last name' (relatively crude)
+			.sort(function( a, b ) {
+				return a.name.split( " " ).pop().toLowerCase() <
+					b.name.split( " " ).pop().toLowerCase() ?
+					-1 : 1;
+			});
 
 		// Handle "legacy" content - content authored outside of the learn site
 		// and attributed with metadata in the file,
 		// push those contributors to the front of the list
 		if ( post.attribution ) {
-			post.attribution.forEach(function(str, idx) {
-				var contrib, m;
+			post.attribution.forEach(function( contributor ) {
+				var contrib, matches;
 
-				// Handling specifically for articles originally from jQuery Fundamentals
-				if (str == "jQuery Fundamentals") {
-					contribs.unshift({
-						name: str,
-						// Use the jQuery Gravatar
-						email: "github@jquery.com",
-						source: post.source
-					});
+				if ( contributor === "jQuery Fundamentals" ) {
+					contrib = {
+						name: "jQuery Fundamentals",
+						email: "github@jquery.com"
+					};
 				} else {
-					m = parseRE.exec(str);
-					if ( m ) {
-						contrib = { name: m[1].trim(), email: m[2] };
-					}
-					else {
-						contrib = { name: str };
-					}
-					if ( post.source ) {
-						contrib.source = post.source;
-					}
-					contribs.unshift( contrib );
+					matches = parseRE.exec( contributor );
+					contrib = {
+						name: matches[ 1 ].trim(),
+						email: matches[ 2 ]
+					};
 				}
+
+				if ( post.source ) {
+					contrib.source = post.source;
+				}
+
+				contribs.unshift( contrib );
 			});
 		}
 
-		if ( post.customFields ) {
-			post.customFields.push({
-				key: "contributors",
-				value: JSON.stringify( contribs )
-			});
-		} else {
-			post.customFields = [{
-				key: "contributors",
-				value: JSON.stringify( contribs )
-			}];
-		}
+		post.customFields = post.customFields || [];
+		post.customFields.push({
+			key: "contributors",
+			value: JSON.stringify( contribs )
+		});
 
-		fn();
+		callback( null );
 	});
-
-});
+}
 
 grunt.registerHelper( "build-pages-preprocess", (function() {
-	var orderMap = grunt.helper( "read-order", "order.json" );
+	var orderMap = getOrderMap();
 
 	return function( post, fileName, done ) {
-		grunt.utils.async.series([
-			function applyOrder( fn ) {
-				var slug = fileName.replace( /^.+?\/(.+)\.\w+$/, "$1" ),
-				menuOrder = orderMap[ slug ];
-				if ( menuOrder ) {
-					post.menuOrder = menuOrder;
-				}
-				fn();
-			},
+		var slug = fileName.replace( /^.+?\/(.+)\.\w+$/, "$1" ),
+			menuOrder = orderMap[ slug ];
 
-			function applyContribs( fn ) {
-				grunt.helper( "contributor-attribution", post, fileName, fn );
-			}
-		], done );
+		if ( menuOrder ) {
+			post.menuOrder = menuOrder;
+		}
+
+		contributorAttribution( post, fileName, done );
 	};
 })());
 
